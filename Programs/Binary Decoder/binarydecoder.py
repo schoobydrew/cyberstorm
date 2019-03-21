3a2fe421eb76d1a5cebfbf0b1ee85c83b9a7f0a2
lrc035 <37704259+lrc035@users.noreply.github.com>
2019-03-21 16:35:57 -0500

    Updated binarydecoder.py

--- Programs/Binary Decoder/binarydecoder.py
+++ Programs/Binary Decoder/binarydecoder.py
@@ -11,15 +11,34 @@ import sys
 # Output: a decoded message
 def decode(bitNum, encodedMsg):
 	# for the length of the encodedMsg, decode 7 or 8 bits at a time
-		# translate the 7 or 8 bits into a character
+	i = 0
+	asciiCode = 0
+	decodedMessage = ""
+	currentbits = 0
+	while (i < len(encodedMsg)):
+		# break into a bit word of correct length
+		currentBits = encodedMsg[i:i+bitNum]
+		# translate binary to decimal ASCII code
+		asciiCode = int(currentBits, 2)
 		# if a backspace, delete last character in message string
+		if (len(decodedMessage) != 0 and asciiCode == 8):
+			decodedMessage = decodedMessage[:-1]
 		# otherwise add character to message string
+		decodedMessage += chr(asciiCode)
+		i += bitNum
 	# return decoded message
+	return decodedMessage
+
 
 # Main Function
+if (__name__ == "__main__"):
 	# retrieve a string from stdin
-	# if length of string divisble by 7,
-	# call decode using 7 as bitNum
-	# if 8 length of string divisible by 8,
-	# call decode using 8 as bitNum
-	# print decoded message
+	binaryMessage = raw_input()
+	# if length of string divisble by 7
+	if (len(binaryMessage)%7 == 0):
+		# call decode using 7 as bitNum
+		print decode(7, binaryMessage)
+	# if length of string divisible by 8
+	if (len(binaryMessage)%8 == 0):
+		# call decode using 8 as bitNum
+		print decode(8, binaryMessage)
