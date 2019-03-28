5bd94c320a7c923319b13fa15115654b40285407
nirjalaparajuli <parajulinirjala@gmail.com>
2019-03-27 23:26:21 -0500

    Add starting point for the program

--- Programs/Binary Decoder/binaryConv.py
+++ Programs/Binary Decoder/binaryConv.py
@@ -1,2 +1,13 @@
-def decode(bitlength=8, message):
-	return ''.join(chr(int(s[i*bitlength:i*bitlength+bitlength],2)) for i in range(len(message)//bitlength))
+def decode(message,bitlength=8):
+	return ''.join(chr(int(message[i*bitlength:i*bitlength+bitlength],2)) for i in range(len(message)//bitlength))
+
+
+if __name__ == '__main__':
+	message = input('Input a Binary message:\n')
+	if (len(message)%7 == 0) and (len(message)%8 ==0):
+		print(decode(message))
+		print(decode(message,7))
+	elif (len(message)%7 == 0):
+		print(decode(message,7))
+	elif (len(message)%8 == 0):
+		print(decode(message))
