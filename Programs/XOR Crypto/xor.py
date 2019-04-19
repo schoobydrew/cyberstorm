8fb194974f52e67e813a386f2733636b148cd8b6
schoobydrew <andrew.schoo@outlook.com>
2019-04-19 16:58:24 -0500

    wrote xor.py and gave functionality in case key is shorter than incoming text
    Please enter the commit message for your changes. Lines starting

---
+++ Programs/XOR Crypto/xor.py
@@ -0,0 +1,18 @@
+#Team - Persians
+#github.com/schoobydrew/cyberstorm
+import sys
+keyfile = "key2"
+def runXOR(text, key):
+    xorText = ''
+    for i in range(len(text)):
+        j = text[i]
+        k = key[i%len(key)]
+        bitwise = (ord(j) ^ ord(k))
+        xorText += chr(bitwise)
+    return xorText
+if (__name__ == "__main__"):
+    #get text from stdin
+    input = sys.stdin.read()
+    #get key from hardcoded key file
+    key = open(keyfile, "r").read()
+    print runXOR(input, key)
