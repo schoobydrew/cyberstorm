2770e91b416e958807a8c20800b0070493d16590
schoobydrew <andrew.schoo@outlook.com>
2019-04-23 19:19:25 -0500

    adding timelock

--- Programs/Steg/steg.py
+++ Programs/Steg/steg.py
@@ -2,7 +2,7 @@
 #github.com/schoobydrew/cyberstorm
 import sys
 #globals
-sentinel = [0x0, 0xff, 0x0, 0x0, 0xff, 0x0]
+sentinel = [chr(0x0),chr(0xFF),chr(0x0),chr(0x0),chr(0xFF),chr(0x0)]
 BIT = False
 BYTE = False
 OFFSET = 0
@@ -13,39 +13,63 @@ RETRIEVE = False
 WRAPPER = ""
 HIDDEN = ""
 #functions
+def printFile(arr):
+    for x in arr:
+        print x
+def getFileData(fileName):
+    data = open(fileName, "rb").read()
+    return data
 def doSteg():
     return object
 def doRetrieve():
-    return object
+    publicFile = getFileData(WRAPPER)
+    hiddenFile = []
+    while (sentinel != hiddenFile[-len(sentinel):]):
+        if (BIT):
+            pass
+        elif (BYTE):
+            hiddenFile.append(chr(publicFile[OFFSET]))
+            OFFSET += INTERVAL
+    hiddenFile = hiddenFile[:-len(sentinel)]
+    return hiddenFile
 #main
-if (__name__ == "__main__" ):
+if (__name__ == "__main__"):
     for arg in sys.argv[1:]:
         if (arg[1] == "b"):
             BIT = True
+            print "BIT: {}".format(BIT)
             #sets to bit mode
         if (arg[1] == "B"):
             BYTE = True
+            print "BYTE: {}".format(BYTE)
             #sets to byte mode
         if (arg[1] == "s"):
             STEG = True
+            print "STEG: {}".format(STEG)
             #sets to encoding mode
         if (arg[1] == "r"):
             RETRIEVE = True
+            print "RETREIVE: {}".format(RETRIEVE)
             #sets to decoding mode
         if (arg[1] == "o"):
             OFFSET = int(arg[2:])
+            print "OFFSET: {}".format(OFFSET)
             #sets the offset
         if (arg[1] == "i"):
             INTERVAL = int(arg[2:])
+            print "INTERVAL: {}".format(INTERVAL)
             #sets the interval
         if (arg[1] == "w"):
             WRAPPER = arg[2:]
+            print "WRAPPER: {}".format(WRAPPER)
             #wrapper file
         if (arg[1] == "h"):
+            print "HIDDEN: {}".format(HIDDEN)
             HIDDEN = arg[2:]
             #sets the hidden file
-        if (arg[1] == "e"):
+        if (arg[1] == "?"):
             LEFT = True
+            print "Right>Left: {}".format(LEFT)
             #sets left to right -> right to left
     if (BIT and BYTE):
         print "Cannot use both bit and byte method"
@@ -62,4 +86,5 @@ if (__name__ == "__main__" ):
     if (STEG):
         print doSteg()
     elif (RETRIEVE):
-        print doRetrieve()
+        hiddenFile = doRetrieve()
+        printFile(hiddenFile)
---
+++ Programs/Timelock/timelock.py
@@ -0,0 +1,48 @@
+#Team Persians
+#Timelock
+#github.com/schoobydrew/cyberstorm
+from hashlib import md5
+import datetime
+import sys
+import time
+#global for manual time entering
+currentTime = "2015 05 15 14 00 00"
+def getTime():
+    if (currentTime != ""):
+        time = currentTime.split(" ")
+        time = [int(x) for x in time]
+        time = datetime.datetime(time[0],time[1],time[2],time[3],time[4],time[5])
+    else:
+        time = datetime.datetime.now()
+    return time
+def getEpoch():
+    time = raw_input()
+    time = time.split(" ")
+    time = [int(x) for x in time]
+    time = datetime.datetime(time[0],time[1],time[2],time[3],time[4],time[5])
+    return time
+def getCode(delta):
+    #set number of numbers and letters in the code according to algorithm
+    digest = md5(md5(str(delta)).hexdigest()).hexdigest()
+    letters = [c for c in digest if c.isalpha()]
+    numbers = [c for c in digest if c.isdigit()]
+    numbers.reverse()
+    code = ""
+    codeLet = "".join(letters)
+    codeNum = "".join(numbers)
+    if (len(codeLet) < 2):
+        code += codeLet
+        code += codeNum[:(4-len(codeLet))]
+    elif (len(codeNum) < 2):
+        code += codeLet[:2]
+        code += codeNum
+        code += codeLet[2:4-len(codeNum)]
+    else:
+        code += codeLet[:2] + codeNum[:2]
+    return code
+if (__name__ == "__main__"):
+    epoch = getEpoch()
+    currTime = getTime()
+    delta = (currTime-epoch).total_seconds()
+    delta = int(((delta//60)*60)-3600)
+    print getCode(delta)
