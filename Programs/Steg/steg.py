e044ab57ee537b52c9e0f970ebbe68f706cbdeb7
schoobydrew <andrew.schoo@outlook.com>
2019-05-01 23:10:05 -0500

    bit method retrieval... I'll work on storage tomorrow, ive got the hang of this now

--- Programs/Steg/steg.py
+++ Programs/Steg/steg.py
@@ -13,6 +13,7 @@ ENDIAN = ""
 sentinel = [0x0, 0xff, 0x0, 0x0, 0xff, 0x0]
 #retrieve Function
 def retrieve(METHOD, OFFSET, INTERVAL, WRAPPER):
+    global sentinel
     source = open(WRAPPER, "rb")
     i = 0
     data = []
@@ -20,10 +21,10 @@ def retrieve(METHOD, OFFSET, INTERVAL, WRAPPER):
     dataSet = ""
     if (METHOD == "B"):
         temp = source.read(1)
-        while (temp != "" and sentinel != data):
+        while (temp != "" and data != sentinel):
             if (bytes == OFFSET + i*INTERVAL):
                 if (len(data) == len(sentinel)):
-                    data.pop(0)
+                    data = data[1:]
                 data.append(ord(temp))
                 dataSet += temp
                 i += 1
@@ -32,7 +33,27 @@ def retrieve(METHOD, OFFSET, INTERVAL, WRAPPER):
         source.close()
         return dataSet
     if (METHOD == "b"):
-        pass
+        temp = source.read(1)
+        byte = ''
+        while (temp != "" and data != sentinel):
+            if (bytes == OFFSET + i*8*INTERVAL + len(byte)*INTERVAL):
+                temp = bin(ord(temp))[2:]
+                #im pretty sure that endian will determine how we append bits to byte
+                ##if im the program works fine without endian
+                if (ENDIAN):
+                    byte = temp[len(temp)-1] + byte
+                else:
+                    byte += temp[len(temp)-1]
+                if (len(byte) == 8):
+                    if (len(data) == len(sentinel)):
+                        data = data[1:]
+                    data.append(int(byte, 2))
+                    dataSet += chr(int(byte, 2))
+                    byte = ""
+                    i += 1
+            bytes += 1
+            temp = source.read(1)
+        return dataSet
 for arg in sys.argv[1:]:
         if ((arg[1] == "b") or (arg[1] == "B")):
             METHOD = arg[1]
