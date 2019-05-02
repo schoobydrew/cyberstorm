2413f1c5067e78bba5c7d28c311e47e85205b2cb
schoobydrew <andrew.schoo@outlook.com>
2019-05-01 22:41:37 -0500

    updated byte method

--- Programs/Steg/steg.py
+++ Programs/Steg/steg.py
@@ -9,78 +9,58 @@ OFFSET = 1
 INTERVAL = 1
 WRAPPER = ""
 HIDDEN = ""
-
-
-def retrieve():
-    # open the file and save the contents into an array
-    with open(WRAPPER, 'rb') as f:
-        contents = f.read()
-
-    i = OFFSET
-    output = ''
-    # Byte method
-    if (METHOD == 'B'):
-        while (i < len(contents)):
-            output += contents[i]
-            # check to see if sentinal values have been found
-            if (len(output) >= 12):
-                if (hexlify(output[-6:]) == "00ff0000ff00"):
-                    # print the output without the sentinal values
-                    print output[:-6]
-                    break
-            i += INTERVAL
-
-    # bit method
-    elif (METHOD == 'b'):
-        print bin(int(hexlify(contents[i]), 16))[2:].zfill(8)
-        while (i < len(contents)):
-            #output += (contents[i] & 1)
-            i += INTERVAL
-
-def store():
-    pass
-
-
-
-if (__name__ == "__main__"):
-    # obtain command line arguments  
-    args = sys.argv
-    # store method
-    METHOD = args[1]
-    METHOD = METHOD[1:]
-    # store action
-    ACTION = args[2]
-    ACTION = ACTION[1:]
-    # store offset
-    OFFSET = args[3]
-    OFFSET = int(OFFSET[2:])
-    # store optional interval value
-    if ('-i' in args[4]):
-        INTERVAL = args[4]
-        INTERVAL = int(INTERVAL[2:])
-    else:
-        # store wrapper file
-        WRAPPER = args[4]
-        WRAPPER = WRAPPER[2:]
-    # at this point, if no optional arguments are used,
-    # then there could no longer be any arguments left
-    if (len(args) >= 6):
-        if ('-w' in args[5]):
-            # store wrapper file
-            WRAPPER = args[5]
-            WRAPPER = WRAPPER[2:]
-        else:
-            # store optional hidden file
-            HIDDEN = args[5]
-            HIDDEN = args[2:]
-    if (len(args) == 7):
-        # store optional hidden file
-        HIDDEN = args[6]
-        HIDDEN = HIDDEN[2:]
-    
-    if (ACTION == 'r'):
-        retrieve()
-    elif (ACTION == 's'):
-        store()
-
-
+ENDIAN = ""
+sentinel = [0x0, 0xff, 0x0, 0x0, 0xff, 0x0]
+#retrieve Function
+def retrieve(METHOD, OFFSET, INTERVAL, WRAPPER):
+    source = open(WRAPPER, "rb")
+    i = 0
+    data = []
+    bytes = 0
+    dataSet = ""
+    if (METHOD == "B"):
+        temp = source.read(1)
+        while (temp != "" and sentinel != data):
+            if (bytes == OFFSET + i*INTERVAL):
+                if (len(data) == len(sentinel)):
+                    data.pop(0)
+                data.append(ord(temp))
+                dataSet += temp
+                i += 1
+            bytes += 1
+            temp = source.read(1)
+        source.close()
+        return dataSet
+    if (METHOD == "b"):
+        pass
+for arg in sys.argv[1:]:
+        if ((arg[1] == "b") or (arg[1] == "B")):
+            METHOD = arg[1]
+            #print "METHOD: {}".format(METHOD)
+            #sets to bit mode
+        if (arg[1] == "s") or (arg[1] == "r"):
+            ACTION = arg[1]
+            #print "Action: {}".format(ACTION)
+            #sets to encoding mode or storage mode
+        if (arg[1] == "o"):
+            OFFSET = int(arg[2:])
+            #print "OFFSET: {}".format(OFFSET)
+            #sets the offset
+        if (arg[1] == "i"):
+            INTERVAL = int(arg[2:])
+            #print "INTERVAL: {}".format(INTERVAL)
+            #sets the interval
+        if (arg[1] == "w"):
+            WRAPPER = arg[2:]
+            #print "WRAPPER: {}".format(WRAPPER)
+            #wrapper file
+        if (arg[1] == "h"):
+            #print "HIDDEN: {}".format(HIDDEN)
+            HIDDEN = arg[2:]
+            #sets the hidden file
+        if (arg[1] == "e"):
+            ENDIAN = arg[1]
+if (ACTION == 'r'):
+    print retrieve(METHOD, OFFSET, INTERVAL, WRAPPER)
+elif (ACTION == 's'):
+    store(METHOD, OFFSET, INTERVAL, WRAPPER, HIDDEN)
